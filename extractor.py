2214e3d586ce4e8f98333ba7733d011e99a85a72
Neri <45918587+neripinata@users.noreply.github.com>
2019-12-27 16:06:34 -0600

    Actualization

--- extractor.py
+++ extractor.py
@@ -13,22 +13,23 @@ for x in files:
         #x = str(x)
         query = x.rstrip(".mp3")
         print(query)
-        for j in search(query, tld="co.in", num=10, start=1, stop=30, pause=2):
+        for j in search(query, tld="co.in", num=10, start=1, stop=15, pause=2):
             if "www.letras.com" in j:
                 try:
-                    # response = requests.get(j)
-                    # print(j)
-                    # print (response.status_code)
-                    # info = response.content
-                    
-                    # info = str(info)
-                    # count = info.count(substring)
-                    #print(info)
-                    
-                    info = look.Extraer(j)
+                    info = look.Extraer_from_letras(j)
                     print(info)
 
                 except:
                     print("Error connection")
+            elif "www.lyrics.com" in j:
+                try:
+                    info = look.Extraer_from_lyrics(j)
+                    print(info)
+
+                except:
+                    print("Error connection")
+
+
+                    
 
 #print(info)
--- look.py
+++ look.py
@@ -3,6 +3,7 @@
 # import re
 # from bs4 import BeautifulSoup
 
+##for www.lyrics.com
 
 # url = 'https://www.lyrics.com/lyric/26337015/Beach+House/Myth'
 
@@ -65,9 +66,8 @@ import re
 from bs4 import BeautifulSoup
 
 
-url = 'https://www.letras.com/muse/71970/'
-
-def Extraer(url):
+def Extraer_from_letras(url):
+    #to look in letras.com
     response = urllib.request.urlopen(url)
     webContent = response.read().decode()
 
@@ -83,7 +83,32 @@ def Extraer(url):
         Artist = x.text
         break
 
+    Artist = Artist.strip()
+    info = [nameSong, Artist]
+
+    return info
+
+    
+
+def Extraer_from_lyrics(url):
+    #to look in lyrics.com
+    response = urllib.request.urlopen(url)
+    webContent = response.read().decode()
+
+    soup = BeautifulSoup(webContent)
+
+    nameSong = soup.h1.string
+
+    Artist = soup.h3.subheading
+
+    for x in soup.find_all('h3'):
+        Artist = x.text
+        Artist = Artist.rstrip(" \xa0Buy This Song\n")
+        break
+
 
     info = [nameSong, Artist]
 
     return info
+
+    
