c4935c7aa295468c88e46b3b4b0aefc1539cf4dd
Neri <45918587+neripinata@users.noreply.github.com>
2019-12-27 16:33:32 -0600

    Search also in DuckDuckGo
    Search also in DuckDuckGo in case error 429 ocurrs with google

--- extractor.py
+++ extractor.py
@@ -2,6 +2,7 @@ from googlesearch import search
 import os
 import requests
 import look
+import ddg3
 
 
 files = os.listdir("songs/")
@@ -13,21 +14,40 @@ for x in files:
         #x = str(x)
         query = x.rstrip(".mp3")
         print(query)
-        for j in search(query, tld="co.in", num=10, start=1, stop=15, pause=2):
-            if "www.letras.com" in j:
-                try:
-                    info = look.Extraer_from_letras(j)
-                    print(info)
+        try:
+            for j in search(query, tld="co.in", num=10, start=1, stop=15, pause=2):
+                if "www.letras.com" in j:
+                    try:
+                        info = look.Extraer_from_letras(j)
+                        print(info)
 
-                except:
-                    print("Error connection")
-            elif "www.lyrics.com" in j:
-                try:
-                    info = look.Extraer_from_lyrics(j)
-                    print(info)
+                    except:
+                        print("Error connection")
+                elif "www.lyrics.com" in j:
+                    try:
+                        info = look.Extraer_from_lyrics(j)
+                        print(info)
 
+                    except:
+                        print("Error connection")
+        except:
+            i = 0
+            while i < 20:
+                print(query)
+                link = ddg3.query(query)
+                print(link.related[i].url)
+                try:
+                    if "www.letras.com" in link.related[i].url:
+                        try:
+                            info = look.Extraer_from_letras(link.related[i].url)
+                            print(info)
+                        except:
+                            print ("Error connection")
                 except:
-                    print("Error connection")
+                    print("nop")
+                i += 1
+
+
 
 
                     
